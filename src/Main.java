f802d4d53c100ad91deb96f18e6a1f1a36568163
Zhumadi1 <dzumaabdykadyrov@gmail.com>
2023-01-17 18:22:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // kiyinki sistema jany joldon bashtalat
+        // System.out.println("Hello world");
+        // System.out.println("Hello world");
+
+        // bir strokaga jazylat
+        // System.out.print("Hello ");
+        // System.out.print("Hello ");
+
+        // %s bul ozuno stroka(sozdordu) kably alat
+        System.out.printf("My name is %s","Zhumadil");
+
+        // %d bul ozuno butun sandardy kabyl alat
+        System.out.printf(" my age %d",23);
+
+        // %f bul ozuno bolchok sandardy kabyl alat (%.2f tochkadan diyinki sandar uchun)
+        System.out.printf(" My weight %f",70.5);
+        System.out.printf(" My weight %.1f",70.5);
+
+        // %c bul ozuno char dy alat
+        System.out.printf(" char %c",'$');
+        // misaly(primer)
+        System.out.printf("My name is %s  my age %d  my weight %f  char %c","Zhumadil",23,70.5,'$');
+
+        /* ozgormolor */
+        //butun sandar uchun
+
+        // byte = 1 bytes -128 to 127
+        byte byteSan = -128;
+        // short = 2 bytes -32,768 to 32,767
+        // int = 4 bytes
+        // long = 8 tytes sandyb akyryna "l"di koshup jazu kerek
+        long longsan = 8937459873l;
+
+        /* bolchok sandar uchun */
+
+        // float 4 bytes
+        float floatSan = 23.223f;
+        // double 8 bytes
+
+        boolean b = true; // tuura
+        boolean b2 = false; // tuura emes
+
+        char a = '$'; // simvoldor uchun
+
+        // scanner
+       // public static void main(String[] args) {
+           // Scanner sc = new Scanner(System.in);
+           // int a = sc.nextInt();
+
+           // System.out.println("Scannerden alyngan san; "+a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
